457b8dbb2e3a876d872e95be69995560014f8ded
Steve Lukas <slukas@gmail.com>
2015-02-06 06:05:06 -0800

    whitespacegone

--- GitItNow/AppDelegate.swift
+++ GitItNow/AppDelegate.swift
@@ -12,6 +12,7 @@ import CoreData
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
